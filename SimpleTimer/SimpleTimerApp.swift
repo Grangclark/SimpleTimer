9955386572af9c269e8aa05d44ded87c17741226
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-01-21 12:16:34 +0900

    Initial Commit

---
+++ SimpleTimer/SimpleTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTimerApp.swift
+//  SimpleTimer
+//
+//  Created by 長橋和敏 on 2025/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
